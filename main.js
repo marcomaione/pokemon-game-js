7aff36be80ba41b94d66898af74840583b2e3252
marco maione <maionemarco14@gmail.com>
2022-10-10 21:02:14 +0200

    aggiunta del muro invisibile

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const canvas = document.querySelector('canvas');
 
 const c = canvas.getContext('2d');
 
+console.log(muro);
+
 //  misure in pixel alla mappa di gioco
 canvas.width = 1024
 canvas.height = 576
@@ -13,7 +15,7 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 // importo la mappa ed il person di gioco
 
 const image = new Image();
-image.src = './img/mappa pokemon.png'
+image.src = './img/mappa.png'
 
 const playerImage = new Image();
 playerImage.src = './img/playerDown.png'
@@ -25,14 +27,14 @@ class Sprite {
         this.image = image
     }
     draw() {
-    c.drawImage(this.image, -640, -450)
+    c.drawImage(this.image, this.position.x, this.position.y)
     }
 }
 
 const background = new Sprite({
     position: {
-    x: -640,
-    y: -450
+    x: -590,
+    y: -300
     },
     image: image
 })
@@ -67,35 +69,64 @@ function animate() {
         playerImage.width / 4,
         playerImage.height
     )
-    // if () 
+    //  if (keys.w.pressed) background.position.y = background.position.y + 3// formula non abbreviata per muovere il personaggio
+    if (keys.w.pressed && lastKey === 'w') background.position.y += 3
+    else if (keys.a.pressed && lastKey === 'a') background.position.x += 3
+    else if (keys.s.pressed && lastKey === 's') background.position.y -= 3
+    else if (keys.d.pressed && lastKey === 'd') background.position.x -= 3 
 }
 animate()
 
 
 // aggiungo la tastiera al gioco
-
+let lastKey = ''
 window.addEventListener('keydown', (e) => {
     
     switch (e.key) {
         case 'w':
             keys.w.pressed = true
+            lastKey = 'w'
         break
 
         case 'a':
             keys.a.pressed = true
+            lastKey = 'a'
         break
 
         case 's':
             keys.s.pressed = true
+            lastKey = 's'
         break
 
         case 'd':
             keys.d.pressed = true
+            lastKey = 'd'
         break
     }
     console.log(keys);
 })
 
+window.addEventListener('keyup', (e) => {
+    
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = false
+        break
+
+        case 'a':
+            keys.a.pressed = false
+        break
+
+        case 's':
+            keys.s.pressed = false
+        break
+
+        case 'd':
+            keys.d.pressed = false
+        break
+    }
+    console.log(keys);
+})
 
 
 
