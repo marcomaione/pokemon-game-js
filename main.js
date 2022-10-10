d4b6b57b8d9023c05d0fb952b1c7eeffb607b2da
marco maione <maionemarco14@gmail.com>
2022-10-10 17:05:59 +0200

    aggiornamento

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const canvas = document.querySelector('canvas');
 
 const c = canvas.getContext('2d');
 
-// do misure in pixel alla mappa di gioco
+//  misure in pixel alla mappa di gioco
 canvas.width = 1024
 canvas.height = 576
 
@@ -18,8 +18,30 @@ image.src = './img/mappa pokemon.png'
 const playerImage = new Image();
 playerImage.src = './img/playerDown.png'
 
-image.onload = () => {
-    c.drawImage(image, -640, -450)
+// creo una classe per il movimento del personaggio
+class Sprite {
+    constructor({position, velocity, image}) {
+        this.position = position
+        this.image = image
+    }
+    draw() {
+    c.drawImage(this.image, -640, -450)
+    }
+}
+
+const background = new Sprite({
+    position: {
+    x: -640,
+    y: -450
+    },
+    image: image
+})
+
+// vado ad posizionare il player sulla mappa e avvio loop animazione personaggio 
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    background.draw()
     c.drawImage(
         playerImage,
         0,
@@ -30,8 +52,34 @@ image.onload = () => {
         canvas.height / 2 - playerImage.height / 2,
         playerImage.width / 4,
         playerImage.height
-    );
+    )
+    
 }
+animate()
+
+
+// aggiungo la tastiera al gioco
+
+window.addEventListener('keydown', (e) => {
+    
+    switch (e.key) {
+        case 'w':
+            console.log('premuto w key')
+        break
+
+        case 'a':
+            console.log('premuto a key')
+        break
+
+        case 's':
+            console.log('premuto s key')
+        break
+
+        case 'd':
+            console.log('premuto d key')
+        break
+    }
+})
 
 
 
