a48fb9170e97deedae5f59308a8e61087cb31b53
marco maione <maionemarco14@gmail.com>
2022-10-13 12:51:05 +0200

    aggiornamento animazione combattimento

---
+++ data/attacks.js
@@ -0,0 +1,12 @@
+const attacks = {
+    Tackle:{
+        name: 'tackle',
+        damage: 10,
+        type: 'normal'
+    },
+    Fireball:{
+        name: 'Fireball',
+        damage: 30,
+        type: 'Fire'
+    }
+}
--- main.js
+++ main.js
@@ -21,7 +21,8 @@ class Sprite {
         image,
         frames = { max: 1, hold: 10},
         sprites,
-        animate = false  
+        animate = false,
+        isEnemy = false
         }) {
         this.position = position
         this.image = image
@@ -33,6 +34,8 @@ class Sprite {
         this.animate = animate
         this.sprites = sprites
         this.opacity = 1
+        this.health = 100
+        this.isEnemy = isEnemy
     }
     draw() {
         c.save()
@@ -62,12 +65,22 @@ class Sprite {
     attack({attack, recipient}) {
         const tl = gsap.timeline()
 
+        this.health -= attack.damage
+        let movementDistance = 20
+        if ( this.isEnemy) movementDistance = -20
+        let healthBar = '#barra-salute-enemy'
+        if (this.isEnemy) healthBar = '#barra-salute'
+
         tl.to(this.position, {
-            x: this.position.x - 20
+            x: this.position.x - movementDistance
         }).to(this.position, {
-            x: this.position.x + 40,
+            x: this.position.x + movementDistance * 2,
             duration: 0.1,
-            onComplete() {
+            onComplete: () => {
+                // animazione vita che scende dopo il colpo
+                gsap.to(healthBar, {
+                    width: this.health + '%'
+                })
                 gsap.to(recipient.position, {
                     x: recipient.position.x + 10,
                     yoyo: true,
@@ -418,7 +431,8 @@ const draggle = new Sprite({
        max: 4,
        hold: 30
     },
-    animate: true
+    animate: true,
+    isEnemy: true
 })
 
 const embyImage = new Image()
@@ -445,15 +459,15 @@ function animateBattle() {
 // animate()
 animateBattle()
 
+//ascoltatori di eventi per i nostri pulsanti(attacco)
 document.querySelectorAll('button').forEach((button) =>{
-    button.addEventListener('click', () => {
-        emby.attack({ attack: {
-            name: 'tackle',
-            damage: 10,
-            type: 'normal'
-        },
-        recipient: draggle,
-    })
+    button.addEventListener('click', (e) => {
+        // creo una costante che al premere del pulsante usera l'attacco ad esso associato con relativo danno
+        const selectedAttack = attacks[e.currentTarget.innerHTML]
+         emby.attack({
+            attack: selectedAttack,
+         recipient: draggle
+        })
     })
 })
 
